9ac29fdc375b7e468b4c58cf8e5cfec2be460a7a
caolei <1745399028@qq.com>
2026-06-11 14:23:43 +0800

    chore: clean up removed files from workspace restructure

--- ruoyi-vue-lgg-backend/ruoyi-business/src/main/java/com/ruoyi/business/exception/DeletionNotAllowedException.java
+++
@@ -1,9 +0,0 @@
-package com.ruoyi.business.exception;
-
-public class DeletionNotAllowedException extends BaseException {
-
-    public DeletionNotAllowedException(String msg) {
-        super(msg);
-    }
-
-}
--- ruoyi-vue-lgg-backend/ruoyi-business/src/main/java/com/ruoyi/business/exception/OrderBusinessException.java
+++
@@ -1,9 +0,0 @@
-package com.ruoyi.business.exception;
-
-public class OrderBusinessException extends BaseException {
-
-    public OrderBusinessException(String msg) {
-        super(msg);
-    }
-
-}
